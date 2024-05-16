1e863a6947d91cd0a9cb3be309e0f1ca1b8e6b8e
Hasibul Hassan <115937739+hasibult53@users.noreply.github.com>
2024-05-16 09:49:52 +0600

    Update Linear_search_c_program_by_HH.c

--- Linear_search_c_program_by_HH.c
+++ Linear_search_c_program_by_HH.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
 int linear_search(int arr[], int n, int x) {
-  // Loop through each element of the array
   for (int i = 0; i < n; i++) {
-    // If element is found at current index
     if (arr[i] == x) {
       return i;
     }
   }
-
-  // If element not found
   return -1;
 }
-
 int main() {
-  int arr[100]; // Array to store elements (adjust size as needed)
+  int arr[100]; 
   int n, x;
-
   printf("Enter the number of elements in the array: ");
   scanf("%d", &n);
-
   printf("Enter the elements of the array:\n");
   for (int i = 0; i < n; i++) {
     scanf("%d", &arr[i]);
   }
-
   printf("Search: ");
   scanf("%d", &x);
-
   int result = linear_search(arr, n, x);
 
   if (result == -1) {
@@ -35,6 +26,5 @@ int main() {
   } else {
     printf("Element %d is present at index %d\n", x, result);
   }
-
   return 0;
 }
